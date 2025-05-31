fe6c0d0530cfc5a54c39f8c334df3061809af5db
fivaz <s.a.fivaz@gmail.com>
2025-05-31 19:20:43 +0200

    refactor

--- src/components/transaction/TransactionItem.tsx
+++ src/components/transaction/TransactionItem.tsx
@@ -1,10 +1,8 @@
 'use client';
 import { Strong, Text } from '@/components/base/text';
 import { DATE_FORMAT, Transaction } from '@/lib/transaction/transaction.model';
-import { CalendarIcon, ClockIcon, CogIcon, CombineIcon } from 'lucide-react';
-import TransactionForm, {
-	type TransactionFormProps,
-} from '@/components/transaction/transaction-form/TransactionForm';
+import { CalendarIcon, ClockIcon, CogIcon } from 'lucide-react';
+import { type TransactionFormProps } from '@/components/transaction/transaction-form/TransactionForm';
 import TransactionFormButton from '@/components/transaction/transaction-form/TransactionFormButton';
 import { format, parse } from 'date-fns';
 import { useMemo } from 'react';
@@ -31,21 +29,20 @@ export default function TransactionItem({
 
 	return (
 		<li className="flex items-center justify-between bg-white px-3 py-2 dark:bg-gray-700">
-			<div className="flex items-center gap-4 truncate">
-				<Text className="flex items-center gap-2">
+			<div className="flex min-w-0 items-center gap-4">
+				<Text className="flex shrink-0 items-center gap-2">
 					<CalendarIcon className="hidden size-4 shrink-0 md:block" />
 					<span className="block md:hidden">{date.short}</span>
 					<span className="hidden md:block">{date.long}</span>
 				</Text>
-				<Text className="hidden items-center gap-2 md:flex">
+				<Text className="hidden shrink-0 items-center gap-2 md:flex">
 					<ClockIcon className="size-4 shrink-0" />
 					<span className="w-8">{date.time}</span>
 				</Text>
-
-				<Strong className="truncate">{transaction.description}</Strong>
+				<Strong className="min-w-0 flex-1 truncate">{transaction.description}</Strong>
 			</div>
 
-			<div className="flex items-center gap-2">
+			<div className="flex shrink-0 items-center gap-2">
 				<MoneyText>{transaction.amount}</MoneyText>
 
 				<TransactionFormButton
