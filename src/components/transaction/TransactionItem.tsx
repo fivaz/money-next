a5c1361a20beed72313a66ea35afd2cfecfd99fb
fivaz <s.a.fivaz@gmail.com>
2025-05-31 19:19:09 +0200

    refactor

--- src/components/transaction/TransactionItem.tsx
+++ src/components/transaction/TransactionItem.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Text } from '@/components/base/text';
+import { Strong, Text } from '@/components/base/text';
 import { DATE_FORMAT, Transaction } from '@/lib/transaction/transaction.model';
 import { CalendarIcon, ClockIcon, CogIcon, CombineIcon } from 'lucide-react';
 import TransactionForm, {
@@ -32,24 +32,19 @@ export default function TransactionItem({
 	return (
 		<li className="flex items-center justify-between bg-white px-3 py-2 dark:bg-gray-700">
 			<div className="flex items-center gap-4 truncate">
-				<div className="flex gap-2">
-					<Text className="flex items-center gap-2">
-						<CalendarIcon className="hidden size-4 shrink-0 md:block" />
-						<span className="block md:hidden">{date.short}</span>
-						<span className="hidden md:block">{date.long}</span>
-					</Text>
-					<Text className="hidden items-center gap-2 md:flex">
-						<ClockIcon className="size-4 shrink-0" />
-						<span className="w-8">{date.time}</span>
-					</Text>
-				</div>
+				<Text className="flex items-center gap-2">
+					<CalendarIcon className="hidden size-4 shrink-0 md:block" />
+					<span className="block md:hidden">{date.short}</span>
+					<span className="hidden md:block">{date.long}</span>
+				</Text>
+				<Text className="hidden items-center gap-2 md:flex">
+					<ClockIcon className="size-4 shrink-0" />
+					<span className="w-8">{date.time}</span>
+				</Text>
 
-				<div className="flex items-center gap-2 truncate">
-					<Text className="truncate font-medium" v-if="transaction.description">
-						{transaction.description}
-					</Text>
-				</div>
+				<Strong className="truncate">{transaction.description}</Strong>
 			</div>
+
 			<div className="flex items-center gap-2">
 				<MoneyText>{transaction.amount}</MoneyText>
 
