71d9670a072743cd14c685251535e45a0aa2a704
fivaz <31249205+fivaz@users.noreply.github.com>
2025-08-07 21:23:18 +0200

    make it a PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,23 @@ import PromptProvider from '@/components/Prompt/PromptProvider';
 
 export const metadata: Metadata = {
 	title: 'Money',
-	description: '',
+	description: 'An app to manage your finance',
+	generator: 'Next.js',
+	manifest: '/manifest.json',
+	keywords: ['nextjs', 'next14', 'pwa', 'next-pwa'],
+	icons: [
+		{ rel: 'icon', url: '/pwa-64x64.png', sizes: '64x64' },
+		{ rel: 'icon', url: '/pwa-192x192.png', sizes: '192x192' },
+		{ rel: 'icon', url: '/pwa-512x512.png', sizes: '512x512' },
+		{ rel: 'apple-touch-icon', url: '/pwa-192x192.png' },
+	],
 };
 
 export const viewport: Viewport = {
-	themeColor: 'width=device-width, initial-scale=1.0',
+	themeColor: [
+		{ media: '(prefers-color-scheme: light)', color: 'oklch(85.2% 0.199 91.936)' },
+		{ media: '(prefers-color-scheme: dark)', color: 'oklch(82.8% 0.189 84.429)' },
+	],
 };
 
 export default async function RootLayout({
