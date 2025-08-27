f430bf9bfe943d89dc1e2a119ab7f0794176a7ab
fivaz <31249205+fivaz@users.noreply.github.com>
2025-08-27 19:51:03 +0200

    add timezone to budgeted-spent

--- src/lib/source/source.utils-api.ts
+++ src/lib/source/source.utils-api.ts
@@ -1,10 +1,12 @@
 import useSWR from 'swr';
 import { fetcher } from '@/lib/shared/api-client.utils';
+import { getParamsDate } from '@/lib/shared/date.utils';
+import { dateParams } from '@/lib/const';
 
 export const BALANCE_URL = `api/balance`;
 
 export const getBudgetedSpentUrl = (year: number, month: number) =>
-	`api/budgeted-spent?year=${year}&month=${month}`;
+	`api/budgeted-spent?${dateParams(year, month)}`;
 
 export const useActualBalance = () => {
 	const { data } = useSWR<number>(BALANCE_URL, fetcher);
